25cd92250cdd17229b958556ba1feaff03e5519a
Lauren Malley <laurenymalley@gmail.com>
2018-08-28 23:52:23 -0400

    init commit

---
+++ app/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+
+const Menu = props => {
+  return (
+    <div>
+      <h1>Teams</h1>
+      <ul>
+        <li>
+          <Link to="/teams">Teams</Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+const App = props => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Menu} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
